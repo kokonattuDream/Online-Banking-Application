d71e2c420ca7182215e5314fe00fb55277e74b7d
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-04-10 18:23:26 -0400

    Transfer between saving and primary accounts

--- src/main/java/com/kokonatsuDream/userfront/service/TransactionService.java
+++ src/main/java/com/kokonatsuDream/userfront/service/TransactionService.java
@@ -2,7 +2,9 @@ package com.kokonatsuDream.userfront.service;
 
 import java.util.List;
 
+import com.kokonatsuDream.userfront.domain.PrimaryAccount;
 import com.kokonatsuDream.userfront.domain.PrimaryTransaction;
+import com.kokonatsuDream.userfront.domain.SavingsAccount;
 import com.kokonatsuDream.userfront.domain.SavingsTransaction;
 
 public interface TransactionService {
@@ -17,4 +19,7 @@ public interface TransactionService {
 	void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction);
 	
 	void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction);
+	
+	void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception;
+	
 }
--- src/main/java/com/kokonatsuDream/userfront/service/UserServiceImpl/TransactionServiceImpl.java
+++ src/main/java/com/kokonatsuDream/userfront/service/UserServiceImpl/TransactionServiceImpl.java
@@ -1,14 +1,19 @@
 package com.kokonatsuDream.userfront.service.UserServiceImpl;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kokonatsuDream.userfront.Dao.PrimaryAccountDao;
 import com.kokonatsuDream.userfront.Dao.PrimaryTransactionDao;
 import com.kokonatsuDream.userfront.Dao.SavingsAccountDao;
 import com.kokonatsuDream.userfront.Dao.SavingsTransactionDao;
+import com.kokonatsuDream.userfront.domain.PrimaryAccount;
 import com.kokonatsuDream.userfront.domain.PrimaryTransaction;
+import com.kokonatsuDream.userfront.domain.SavingsAccount;
 import com.kokonatsuDream.userfront.domain.SavingsTransaction;
 import com.kokonatsuDream.userfront.domain.User;
 import com.kokonatsuDream.userfront.service.TransactionService;
@@ -26,6 +31,11 @@ public class TransactionServiceImpl implements TransactionService{
 	private PrimaryTransactionDao primaryTransactionDao;
 	@Autowired
 	private SavingsTransactionDao savingsTransactionDao;
+	
+	@Autowired
+	private PrimaryAccountDao primaryAccountDao;
+	@Autowired
+	private SavingsAccountDao savingsAccountDao;
 	
 	public List<PrimaryTransaction> findPrimaryTransactionList(String username){
 		
@@ -59,5 +69,36 @@ public class TransactionServiceImpl implements TransactionService{
 	
 	public void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction) {
 		savingsTransactionDao.save(savingsTransaction);
+	}
+	
+	public void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception{
+		
+		if(transferFrom.equalsIgnoreCase("Primary") && transferTo.equalsIgnoreCase("Savings")) {
+			primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+			savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().add(new BigDecimal(amount)));
+			
+			primaryAccountDao.save(primaryAccount);
+			savingsAccountDao.save(savingsAccount);
+			
+			Date date = new Date();
+			
+			PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Between account transfer from " + transferFrom + " to " + transferTo, "Account", "Finished", Double.parseDouble(amount), primaryAccount.getAccountBalance(), primaryAccount);
+			
+			primaryTransactionDao.save(primaryTransaction);
+		} else if(transferFrom.equalsIgnoreCase("Savings") && transferTo.equalsIgnoreCase("Primary")) {
+			primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().add(new BigDecimal(amount)));
+			savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+			
+			primaryAccountDao.save(primaryAccount);
+			savingsAccountDao.save(savingsAccount);
+			
+			Date date = new Date();
+			
+			SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Between account transfer from " + transferFrom + " to " + transferTo, "Account", "Finished", Double.parseDouble(amount), savingsAccount.getAccountBalance(), savingsAccount);
+			
+			savingsTransactionDao.save(savingsTransaction);
+		} else {
+			throw new Exception("Invalid Transfer");
+		}
 	}
 }
